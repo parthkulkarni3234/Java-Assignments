f196ce985b5202ecf45620c78b393082edcdcb4a
parthkulkarni3234 <parth.kulkarni.btech2025@sitpune.edu.in>
2026-08-08 10:14:54 +0530

    Add files via upload

---
+++ ASG-6/VehicleDemo.java
@@ -0,0 +1,33 @@
+class Vehicle {
+    private String vehicleName = "Tesla Model X";
+    private String vehicleType = "Electric Car";
+
+    // Inner Class
+    class VehicleDetails {
+        void display() {
+            System.out.println("Vehicle Name: " + vehicleName);
+            System.out.println("Vehicle Type: " + vehicleType);
+        }
+    }
+}
+
+interface Action {
+    void performAction();
+}
+
+public class VehicleDemo {
+    public static void main(String[] args) {
+        // Inner Class
+        Vehicle vehicle = new Vehicle();
+        Vehicle.VehicleDetails details = vehicle.new VehicleDetails();
+        details.display();
+
+        // Anonymous Class
+        Action action = new Action() {
+            public void performAction() {
+                System.out.println("Vehicle is starting the engine...");
+            }
+        };
+        action.performAction();
+    }
+}
